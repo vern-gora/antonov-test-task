a19d9390a8465056ebb7755cb7914fd76de9d62e
Danylo <danvern99@gmail.com>
2024-05-17 15:18:03 +0300

    live page

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<BrowserRouter>
+			<BrowserRouter basename='/antonov-test-task'>
 				<App />
 			</BrowserRouter>
 		</Provider>
